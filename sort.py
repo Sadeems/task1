57d4d249756b5492655136906aab8820dd078af3
sarahH12A <144262637+sarahH12A@users.noreply.github.com>
2023-09-07 22:07:37 +0300

    Update sort.py

--- sort.py
+++ sort.py
@@ -15,3 +15,23 @@ size = len(arr)
 selectionSort(arr, size)
 print('The array after sorting in Ascending Order by selection sort is:')
 print(arr)
+
+
+def insertionSort(arr):
+    n = len(arr)  
+      
+    if n <= 1:
+        return  
+ 
+    for i in range(1, n): 
+        key = arr[i]  
+        j = i-1
+        while j >= 0 and key < arr[j]:  
+            arr[j+1] = arr[j]  
+            j -= 1
+        arr[j+1] = key  
+  
+
+arr = [12, 11, 13, 5, 6]
+insertionSort(arr)
+print(arr)
